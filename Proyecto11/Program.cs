1368e6614fa9d6af277fc300717a1caab06b7f12
mozart953 <wilsonnianopop@gmail.com>
2023-05-21 11:02:37 -0600

    proyecto 11

---
+++ Proyecto11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Proyecto11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] postres = { "pay de manzana", "pastel de chocolate", "manzana caramelizada", "fresas con crema", "pay de manzana y pera" };
+
+            bool mayusculas = false;
+
+            IEnumerable<string> resultados;
+
+            var manzanas = postres.Where(n => n.Contains("manzana"));
+            var ordenes = manzanas.OrderBy(n => n);
+
+
+            if (mayusculas)
+            {
+                resultados = ordenes.Select(n => n.ToUpper());
+            }
+            else
+            {
+                resultados = ordenes;
+            }
+
+            foreach (string postre in resultados)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("-----------------");
+
+
+
+        }
+    }
+}
