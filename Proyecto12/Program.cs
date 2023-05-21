3666fa10ff5e985942788f21dd679b0170565d5c
mozart953 <wilsonnianopop@gmail.com>
2023-05-21 13:02:48 -0600

    proyecto 12

---
+++ Proyecto12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Proyecto12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] postres = { "pay de manzana", "pastel de chocolate", "manzana caramelizada", "fresas con crema", "pay de pera y manzana" };
+
+            Console.WriteLine("------- Where ----\r\n");
+
+            IEnumerable<string> r1 = postres.Where((n, i)=>i % 2 == 0);
+
+            foreach (string postre in r1)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("-------------------");
+
+            IEnumerable<string> r2 = from p in postres
+                                     where p.StartsWith("pay")
+                                     select p;
+
+            foreach (string postre in r2)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("-------------------------");
+
+
+
+
+           
+        }
+    }
+}
