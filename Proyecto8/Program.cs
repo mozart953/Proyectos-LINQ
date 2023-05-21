db42101ccb6f414b0d741f4e25d88cf584de999d
mozart953 <wilsonnianopop@gmail.com>
2023-05-20 18:39:32 -0600

    Programa 8:

---
+++ Proyecto8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Proyecto8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 5, 4, 7, 6, 3, 5, 9, 8, 11 };
+
+            IEnumerable<int> pares = numeros.Where(n => n % 2 == 0);
+
+            foreach (int num in pares)
+                Console.WriteLine(num);
+
+            Console.WriteLine("------------------");
+
+
+            string[] postres = { "pay de manzana", "pastel de chocolate", "manzana caramelizada", " fresas con crema" };
+
+            IEnumerable<string> encontrados = postres.Where(p => p.Contains("manzana"));
+
+            foreach (string postre in encontrados)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("----------------");
+
+            IEnumerable<string> manzanas = postres
+                .Where(p => p.Contains("manzana"))
+                .OrderBy(p => p.Length)
+                .Select(p => p.ToUpper());
+
+            foreach (string postre in manzanas)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("------------");
+
+
+        }
+    }
+}
